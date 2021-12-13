d4bea599cb962fd467a04d2cce8296675ff7044e
David <davidphang431@gmail.com>
2021-12-13 14:22:01 +0700

    Initial Commit

---
+++ src/app.ts
@@ -0,0 +1,22 @@
+//? Import dependencies
+import * as dotenv from "dotenv";
+dotenv.config({ path: __dirname + `/.env.${process.env.NODE_ENV}` });
+import express, { Request, Response, NextFunction } from "express";
+
+//? Express Settings
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//? Set Routes
+app.get("/api/v1/check", (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).json({
+    info: "Health check okay!",
+    environment: `${process.env.NODE_ENV}`,
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () =>
+  console.log(`App running on port ${PORT} ${process.env.NODE_ENV} server`)
+);
