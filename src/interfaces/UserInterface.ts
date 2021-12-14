14e779d134e2ce6d6af6b6f59fa3111b2d4a305b
David <davidphang431@gmail.com>
2021-12-14 14:23:52 +0700

    Add user interface and model

---
+++ src/interfaces/UserInterface.ts
@@ -0,0 +1,10 @@
+import { Document, Types } from "mongoose";
+
+export interface UserInterface extends Document {
+  _id: Types.ObjectId;
+  username: string;
+  avatar: string;
+  email: string;
+  password: string;
+  isValid: boolean;
+}
---
+++ src/models/User.ts
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import { UserInterface } from "../interfaces/UserInterface";
+
+const UserSchema = new mongoose.Schema<UserInterface>(
+  {
+    username: {
+      type: String,
+      required: [true, "Username can't be empty"],
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: [true, "Email can't be empty"],
+    },
+    password: {
+      type: String,
+      required: [true, "Password can't be empty"],
+      set: setPassword,
+    },
+    avatar: {
+      type: String,
+      default: "https://res.cloudinary.com/davidph223/image/upload/v1639466353/records-keeper/14-147008_d-d-5th-edition-logo-latest-dd-logo_uvgoj8.png"
+    },
+    isValid: {
+      type: Boolean,
+      default: false
+    },
+  },
+  {
+    timestamps: {
+      createdAt: "createdAt",
+      updatedAt: "updatedAt",
+    },
+    toJSON: {
+      versionKey: false,
+    },
+  }
+);
+
+function setPassword(password: string) {
+    return bcrypt.hashSync(password, 10);
+  }
+  
+
+const User = mongoose.model<UserInterface>("User", UserSchema);
+
+export { User };
