212207ae8af7d47bd98d89f2062cbff7fdbf7990
David <davidphang431@gmail.com>
2021-12-15 15:13:58 +0700

    Add user validator

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,4 +1,3 @@
-import { AuthRequestInterface } from "./auth/AuthRequestInterface";
 import { UserInterface } from "./models/UserInterface";
 
-export { AuthRequestInterface, UserInterface };
+export { UserInterface };
--- src/routes/UserRouter.ts
+++ src/routes/UserRouter.ts
@@ -2,11 +2,18 @@ import express from "express";
 
 import { register, login, authenticateUser } from "../middlewares/auth/auth";
 import { AuthController } from "../controllers/AuthController";
+import { UserValidator } from "../middlewares/validators/UserValidator";
 
 const router = express.Router();
 
-router.post("/register", register, AuthController.getToken);
-router.post("/login", login, AuthController.getToken);
+router.post(
+  "/register",
+  UserValidator.register,
+  register,
+  AuthController.getToken
+);
+
+router.post("/login", UserValidator.login, login, AuthController.getToken);
 router.get("/user", authenticateUser, AuthController.getMe);
 
 export default router;
